0cd02414125c717e383993300be2b18962778946
Vishnu Varthan <vishnulatha006@gmail.com>
2025-02-04 20:11:48 +0530

    handler refac

--- command/handler.go
+++ command/handler.go
@@ -1,9 +1,7 @@
 package command
 
 import (
-	"strconv"
 	"sync"
-	"sync/atomic"
 
 	"github.com/VarthanV/kv-store/pkg/objects"
 	"github.com/VarthanV/kv-store/resp"
@@ -26,7 +24,6 @@ type HandlerFuncMap map[string]HandlerFunc
 
 type Handler struct {
 	mu          sync.Mutex
-	intsets     map[string]*atomic.Int64
 	sets        map[string]string
 	hsets       map[string]map[string]string
 	keyMetaData map[string]string
@@ -35,7 +32,6 @@ type Handler struct {
 func New() *Handler {
 	return &Handler{
 		mu:          sync.Mutex{},
-		intsets:     make(map[string]*atomic.Int64),
 		sets:        make(map[string]string),
 		hsets:       make(map[string]map[string]string),
 		keyMetaData: map[string]string{},
@@ -71,24 +67,10 @@ func (h *Handler) set(args []resp.Value) resp.Value {
 	key := args[0].Bulk
 	value := args[1].Bulk
 
-	// If it is of type integer we can store in the
-	// intset which is helpful in doing atomic INCR and DECR
-	// operations
-	_val, err := strconv.ParseInt(value, 10, 64)
-	if err != nil {
-		logrus.Info("error in converting to integer ", err)
-		// Fallback to normal set
-		h.mu.Lock()
-		h.sets[key] = value
-		h.mu.Unlock()
-		logrus.Debugf("set key %s to value %s in normal set", key, value)
-	} else {
-		h.mu.Lock()
-		var atomicVal atomic.Int64
-		atomicVal.Store(_val)
-		h.intsets[key] = &atomicVal
-		logrus.Debugf("set key %s to value %s in integer set", key, value)
-	}
+	h.mu.Lock()
+	h.sets[key] = value
+	h.mu.Unlock()
+	logrus.Debugf("set key %s to value %s in set", key, value)
 
 	return resp.Value{Typ: objects.SIMPLE_STRING, Str: "OK"}
 }
@@ -104,18 +86,8 @@ func (h *Handler) get(args []resp.Value) resp.Value {
 	h.mu.Lock()
 	defer h.mu.Unlock()
 
-	// Check if it is in the intset
-	_val, ok := h.intsets[key.Bulk]
-	if ok {
-		logrus.Debugf("key %s found in integer set", key.Bulk)
-		h.mu.Unlock()
-		return resp.Value{Typ: objects.INTEGER, Num: int(_val.Load())}
-	}
-
-	// Check if it is in the normal set
 	val, ok := h.sets[key.Bulk]
 	if ok {
-		logrus.Debugf("key %s found in normal set", key.Bulk)
 		h.mu.Unlock()
 		return resp.Value{Typ: objects.BULK_STRING, Bulk: val}
 	}
